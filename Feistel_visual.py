0fcb3490ffce4309ffe8e084eea5881020f5fc4a
JarrLane <lanejarrett313@gmail.com>
2025-02-08 16:34:15 -0500

    Update Feistel_visual.py

--- Feistel_visual.py
+++ Feistel_visual.py
@@ -9,6 +9,7 @@ class fiestelRunEncryption():
         self.key = ""
         self.left = ""
         self.right = ""
+        self.roundsToRun = 0
 
     def plaintext_preparer(self, plaintext):
         try:
@@ -67,11 +68,29 @@ def encrypt():
             return
         else:
             outputLabel.config(text=f"Left: {Run.left}\nRight: {Run.right}\nKey: {Run.key}", fg="green")
+            set_rounds()
+
+    def set_rounds():
+        outputLabel2.config(text="Choose how many rounds you wish to run the cipher")
+        roundOptions = {'4 Rounds': 4, '8 Rounds': 8, '16 Rounds': 16}
+        for text, value in roundOptions.items():
+            radio_button = tkinter.Radiobutton(startScreen, text=text, variable=r, value=value, background="white")
+            radio_button.pack(side=tkinter.TOP, ipady=5)
+
+        beginButton = tkinter.Button(startScreen, text="Begin Encryption", command=setupComplete)
+        beginButton.pack(side=tkinter.TOP, ipady=5)
+
+    def setupComplete():
+        startScreen.withdraw()
+        runningScreen.deiconify()
+        Run.roundsToRun = r.get()
+
+
 
     startScreen = tkinter.Tk()
     startScreen.resizable(True, True)
     startScreen.title("Setup Encryption")
-    startScreen.geometry("400x400")
+    startScreen.geometry("600x600")
     startScreen.config(bg="white")
 
     inputPlaintext = tkinter.Entry(startScreen, width=40)
@@ -81,15 +100,22 @@ def encrypt():
     submitButton.pack(padx=5, pady=5)
     outputLabel = tkinter.Label(startScreen, text="", bg="white")
     outputLabel.pack(pady=10)
+    outputLabel2 = tkinter.Label(startScreen, text="", bg="white")
+    outputLabel2.pack(pady=10)
+    r = tkinter.IntVar()
 
-    startScreen.mainloop()
     runningScreen = tkinter.Tk()
-
-    roundDisplay = tkinter.Label(runningScreen, text="Round " + str(Run.roundNumber))
+    runningScreen.resizable(True, True)
+    runningScreen.title("Encryption")
+    runningScreen.geometry("600x600")
+    roundDisplay = tkinter.Label(runningScreen, text="Round " + str(Run.roundNumber), relief="ridge", bg="white")
     nextRound = tkinter.Button(runningScreen, text="Next Round", command=Run.feistelRound)
+    
     roundDisplay.pack()
     nextRound.pack()
+    runningScreen.withdraw()
+    startScreen.mainloop()
 
-    #runningScreen.mainloop()
+
 encrypt()
 
